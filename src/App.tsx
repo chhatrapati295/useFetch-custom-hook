2def0790f9e19f16411b135673a563f26fba3489
Chhatrapati Chauhan <116446768+chhatrapati295@users.noreply.github.com>
2025-12-09 19:11:24 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <main className="flex flex-col items-center gap-8 py-16 max-w-[1280px] mx-auto">
+      <h1>Use fetch created in a custom hook</h1>
+    </main>
+  );
+}
+
+export default App;
